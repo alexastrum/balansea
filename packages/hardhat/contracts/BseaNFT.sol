0adfbf1d3ee6c3b11543af26d2644d57bb395f3b
Alex <alexastrum@gmail.com>
2024-03-01 19:32:03 -0700

    token contracts

---
+++ packages/hardhat/contracts/BseaNFT.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract BalanceSeaPremiumMembership is ERC721, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    uint256 private _nextTokenId;
+
+    constructor(address defaultAdmin, address minter)
+        ERC721("Balance Sea Premium Membership", "BSEA-PM")
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://TODO/";
+    }
+
+    function safeMint(address to) public onlyRole(MINTER_ROLE) {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
